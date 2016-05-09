8e8324e97bc08003175eafc5e8c7135eda4f8aa3
yeeie201 <lucas20229763@hotmail.com>
2016-05-08 18:49:02 -0700

    style and add new table field in event for admin page use

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160508224408) do
+=======
+ActiveRecord::Schema.define(version: 20160509013954) do
+>>>>>>> style and add new table field in event for admin page use
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160508224408) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.datetime "start_time"
+    t.string   "meetup_group"
   end
 
   create_table "organization_technologies", force: :cascade do |t|
