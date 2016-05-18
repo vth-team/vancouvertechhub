3dfda0b9efc61d6486645eda5d9eadc2fbd7ee3b
Ian Simpson <IDFSimpson@ymail.com>
2016-05-18 12:13:40 -0700

    add rspec test for the display of published and unpublished organization. Unpublished organization should not be seen on the index or on their show page. Only checking for not logged in user

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,52 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+   def unpublished_attributes
+      FactoryGirl.attributes_for(:organization).merge({published: false})
+   end
+
+   def published_attributes
+     FactoryGirl.attributes_for(:organization).merge({published: true})
+   end
+
+   describe "#show" do
+     describe "as anon user" do
+       it "doesn't display unpublished organizations" do
+         org = Organization.new(unpublished_attributes)
+         org.save
+         begin
+           get :show, id: org.id
+         rescue
+           puts "not published"
+           puts org
+         end
+         expect(assigns(:organization)).not_to eq(org)
+       end
+
+       it "displays published organizations show page" do
+         org = Organization.new(published_attributes)
+         org.save
+         get :show, id: org.id
+         expect(assigns(:organization)).to eq(org)
+       end
+     end
+   end
+
+  describe "#index" do
+    describe "as anon user" do
+      it "doesn't display unpublished organizations on index" do
+        org = Organization.new(unpublished_attributes)
+        org.save
+        get :index
+        expect(assigns(:organizations)).not_to include(org)
+      end
+
+      it "displays published organizations on index" do
+       org = Organization.new(published_attributes)
+       org.save
+       get :index
+       expect(assigns(:organizations)).to include(org)
+      end
+    end
+  end
 end
