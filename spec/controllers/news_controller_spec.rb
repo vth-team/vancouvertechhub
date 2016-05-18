4b46600a45d8f5f702b5ece1432d85f050ede9cf
g k <gurbraj.kang@gmail.com>
2016-05-18 12:12:16 -0700

    Add some tests

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe NewsController, type: :controller do
 
+  describe "#index" do
+
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template(:index)
+
+      end
+
+
+  end
+
+  describe "#string_search_terms" do
+
+
+  end
+
+
+
+
+
 end
--- spec/models/news_article_spec.rb
+++ spec/models/news_article_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe NewsArticle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+
+  #   do not know the validations for this one
+  #   it "requires a unique searchterm" do
+  #     #a=NewsFilter.new(FactorGirl.attributes_for(:))
+  #     a=NewsArticle.new(       )
+  #     a.save
+  #     b=NewsArticle.new(        )
+  #     b.save
+  #     expect(b).to be_invalid
+  #   end
+  #
+  #
+  # end
+
+
+
 end
+
+
+
+####test for uniqsnes of title, and presence
