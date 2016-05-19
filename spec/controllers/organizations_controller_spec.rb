fb340f647d270900e4551f61c40a53daf3c6c6e1
Dan Pinto <fozzarelo@gmail.com>
2016-05-19 11:34:27 -0700

    convert tabs to spaces

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 # TODO You should also use FactoryGirl.create rather than User.create with FactoryGirl.attributes_for
 
 RSpec.describe OrganizationsController, type: :controller do
-<<<<<<< HEAD
   let (:unpublished_organization) {FactoryGirl.create(:organization)}
   let (:published_organization) {FactoryGirl.create(:published_organization)}
   let (:admin_user) { FactoryGirl.create(:admin) }
@@ -109,68 +108,4 @@ RSpec.describe OrganizationsController, type: :controller do
       end
     end
   end
-=======
-	let (:unpublished_organization) do
-	  Organization.create(FactoryGirl.attributes_for(:organization).merge({published: false}))
-	end
-
-	let (:published_organization) do
-	  Organization.create(FactoryGirl.attributes_for(:organization).merge({published: true}))
-	end
-
-	let (:admin_user) { User.create(FactoryGirl.attributes_for(:user).merge({admin: true})) }
-
-	describe "#show" do
-		describe "as anon user" do
-			it "doesn't display unpublished organizations" do
-				expect {get :show, id: unpublished_organization.id}.to raise_exception(ActiveRecord::RecordNotFound)
-			end
-
-			it "displays published organizations show page" do
-				get :show, id: published_organization.id
-				expect(assigns(:organization)).to eq(published_organization)
-			end
-		end
-
-		describe "as an admin" do
-			it "display unpublished organizations" do
-				session[:user_id] = admin_user.id
-				get :show, id: unpublished_organization.id
-				expect(assigns(:organization)).to eq(unpublished_organization)
-			end
-			it "display published organizations" do
-			  session[:user_id] = admin_user.id
-			  get :show, id: published_organization.id
-			  expect(assigns(:organization)).to eq(published_organization)
-			end
-		end
-	end
-
-	describe "#index" do
-
-		describe "as anon user" do
-			it "doesn't display unpublished organizations on index" do
-				get :index
-				expect(assigns(:organizations)).not_to include(unpublished_organization)
-			end
-			it "displays published organizations on index" do
-				get :index
-				expect(assigns(:organizations)).to include(published_organization)
-			end
-		end
-
-		describe "as an admin" do
-			it "does display unpublished organizations on index" do
-				session[:user_id] = admin_user.id
-				get :index
-				expect(assigns(:organizations)).to include(unpublished_organization)
-			end
-			it "does display published organizations on index" do
-				session[:user_id] = admin_user.id
-				get :index
-				expect(assigns(:organizations)).to include(published_organization)
-			end
-		end
-	end
->>>>>>> orgs per page set to constant, lets and factorygirl in tests, begin rescue removed from test
 end
