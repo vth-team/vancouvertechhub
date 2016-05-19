7f5dbd092f0c4cc47611c2442d7ffd216473f372
Frank Liu Mac <frankliu81@gmail.com>
2016-05-19 09:57:22 -0700

    Remove google_cse_api.rb in .gitignore

--- spec/models/news_article_spec.rb
+++ spec/models/news_article_spec.rb
@@ -3,24 +3,29 @@ require 'rails_helper'
 RSpec.describe NewsArticle, type: :model do
 
   describe "validations" do
+    it "requires a title" do
+        # GIVEN: campgain with no title
+        n = NewsArticle.new
 
-  #   do not know the validations for this one
-  #   it "requires a unique searchterm" do
-  #     #a=NewsFilter.new(FactorGirl.attributes_for(:))
-  #     a=NewsArticle.new(       )
-  #     a.save
-  #     b=NewsArticle.new(        )
-  #     b.save
-  #     expect(b).to be_invalid
-  #   end
-  #
-  #
+        # WEHN: we validate the campaign
+        validation_outcome = n.valid?
+
+        # THEN: validation_outcome is false
+        expect(validation_outcome).to eq(false)
+
+    end
+
+    it "requires a unique title" do
+
+      NewsArticle.create title: "hello"
+      n1 = NewsArticle.new title: "hello"
+
+      n1.valid?
+
+      expect(n1.errors).to have_key :title
+    end
   end
-
-
-
+  
 end
-
-
 
 ####test for uniqsnes of title, and presence
