a4960a729a2c0fb4010043361ffb9c07073a3036
Jane Wong <jaynellew@gmail.com>
2016-05-20 10:00:47 -0700

    Refactored code according to comments and fixed event time display in calendar

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,19 +1,9 @@
 module EventsHelper
-  def formatted_time_zeropadded(time)
+  def formatted_time(time)
     # TODO: Fix Meetup API scraping
     # Time scraped from rake task is not being parsed correctly
     # UTC time is off by 5 hours, which disallows use of .localtime
     correctTime = time + 12.hours
     correctTime.strftime('%I:%M %p')
-  end
-
-  def formatted_time(time)
-    correctTime = time + 12.hours
-    correctTime.strftime('%l:%M %p')
-  end
-
-  def formatted_date(date)
-    correctDate = date + 12.hours
-    correctDate.strftime('%d %B %Y at %l:%M%p')
   end
 end
