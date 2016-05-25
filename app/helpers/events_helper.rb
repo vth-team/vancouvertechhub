e2f81131417caa0ed3ce9e66721a90eba0da364d
Jane Wong <jaynellew@gmail.com>
2016-05-25 11:22:23 -0700

    Styled Event list and ordered Events by start time

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
-  def formatted_time(time)
+  def formatted_time_zeropadded(time)
     # TODO: Fix Meetup API scraping
     # Time scraped from rake task is not being parsed correctly
     # UTC time is off by 5 hours, which disallows use of .localtime
@@ -7,7 +7,13 @@ module EventsHelper
     correctTime.strftime('%I:%M %p')
   end
 
-  def formatted_date(date)
+  def formatted_time(time)
+    correctTime = time + 12.hours
+    correctTime.strftime('%l:%M %p')
+  end
 
+  def formatted_date(date)
+    correctDate = date + 12.hours
+    correctDate.strftime('%d %B %Y at %l:%M%p')
   end
 end
