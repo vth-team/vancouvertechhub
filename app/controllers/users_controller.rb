78950df437f35a5718c8861e730d0210bd3d0066
Cristian Guerrero <internethostage@example.com>
2016-05-25 11:25:07 -0700

    add ability to set an managed organization from the user edit page (for admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+
   def new
    @user = User.new
-<<<<<<< 63c654688f33e86be28c5c5863cea7f747ecacf6
   end
 
   def edit
-   @user = User.find_by_id params[:id]
-   render "edit"
+    @user = User.find_by_id params[:id]
+    @organizations = unclaimed_organizations
+    @organizations.push @user.organization if @user.organization.present?
+    render "edit"
   end
 
   def create
@@ -18,70 +20,45 @@ class UsersController < ApplicationController
       render :new
     end
   end
-=======
- end
 
-  def edit
-    @organizations = Organization.where(user_id: nil)
+  def update
     @user = User.find_by_id params[:id]
-    # the page is passed in by application.html.erb
-    # via <%= link_to "Edit Profile", edit_user_path(user_id: current_user.id, page: "password_reset") %>
-    render "edit"
+    if can? :manage, @user
+      user_update_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin, :organization_id)
+    else
+      user_update_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    if @user.update user_update_params
+      redirect_to root_path, notice: "Account Updated"
+    else
+      render :edit
+    end
   end
-
- def create
-   # we generate the password_digest automatically
-   # has_secure_password make sure password == password_confirmation
-   user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-   @user = User.new user_params
-
-   if @user.save
-     session[:user_id] = @user.id
-     #redirect_to root_path, notice: "Account Created!"
-     @user.generate_account_verification_data
-     AccountVerificationsMailer.send_verification_instructions(@user).deliver_later
-     render "users/account_verifications/create"
-
-   else
-     render :new
-   end
-
- end
-
->>>>>>> wip, rebasing from parent branch
-
-def update
-  @user = User.find_by_id params[:id]
-  if can? :manage, @user
-    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin, :organization_id)
-  else
-    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
-
-  if @user.update user_params
-    redirect_to root_path, notice: "Account Updated"
-  else
-    render :edit
-  end
-<<<<<<< 63c654688f33e86be28c5c5863cea7f747ecacf6
 
   def destroy
-=======
-end
-
-
- def destroy
->>>>>>> wip, rebasing from parent branch
-   user = User.find_by_id params[:id]
-   session[:user_id] = nil
-   user.destroy
-   redirect_to root_path, notice: "User deleted!"
+    user = User.find_by_id params[:id]
+    session[:user_id] = nil
+    user.destroy
+    redirect_to root_path, notice: "User deleted!"
   end
+
+
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def unclaimed_organizations
+    unclaimed = []
+    Organization.all.each do |org|
+      if org.user.nil?
+        unclaimed.push(org)
+      end
+    end
+    unclaimed
   end
 
 end
