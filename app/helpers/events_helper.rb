5d686faa72d1dc8bb0bab1747b53c85fa7911f4b
Jane Wong <jaynellew@gmail.com>
2016-05-25 10:17:13 -0700

    Added styling to calendar dates and header

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,6 +4,10 @@ module EventsHelper
     # Time scraped from rake task is not being parsed correctly
     # UTC time is off by 5 hours, which disallows use of .localtime
     correctTime = time + 12.hours
-    correctTime.strftime('%I:%M %p') 
+    correctTime.strftime('%I:%M %p')
+  end
+
+  def formatted_date(date)
+
   end
 end
