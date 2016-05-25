86ba08634cfbd67e49a24f64d38ae1b7ec21ee47
Frank Liu Mac <frankliu81@gmail.com>
2016-05-24 22:36:58 -0700

    Fix bug after FetchNewsJob refactor into smaller methods

--- app/jobs/fetch_news_job.rb
+++ app/jobs/fetch_news_job.rb
@@ -2,45 +2,43 @@ class FetchNewsJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    byebug
-    status = false
+    @status = false
     NewsFilter.all.each do |news_filter|
       begin
         search(news_filter)
-      end
       rescue => err
         logger.info(err.message)
       end
     end
-    return status
+    return @status
   end
 
   def search(news_filter)
     @news = GoogleCustomSearchApi.search(news_filter.search_term, page: 1)
     @news.items.each do |item|
-    parser = ArticleParser.new(item)
+      parser = ArticleParser.new(item)
 
-    if parser.published_on
-      article = NewsArticle.new(
-        title: parser.htmlTitle,
-        snippet: parser.htmlSnippet,
-        link: parser.link,
-        thumbnail: parser.thumbnail,
-        published_on: parser.published_on
-      )
-    else
-      Rails.logger = Logger.new(STDOUT)
-      Rails.logger.info "Not a real article"
+      if parser.published_on
+        article = NewsArticle.new(
+          title: parser.htmlTitle,
+          snippet: parser.htmlSnippet,
+          link: parser.link,
+          thumbnail: parser.thumbnail,
+          published_on: parser.published_on
+        )
+      else
+        Rails.logger = Logger.new(STDOUT)
+        Rails.logger.info "Not a real article"
+      end
 
+      if article && article.save
+        @status = true
+        Rails.logger.info "article saved"
+      else
+        @status = false
+        Rails.logger.info "article not saved"
+      end
     end
-
-    if article && article.save
-      status = true
-      Rails.logger.info "article saved"
-    else
-      status = false
-      Rails.logger.info "article not saved"
-    end
-
   end
+
 end
