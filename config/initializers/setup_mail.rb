678d1be6543eb694e9509e41865a0c113f3bfa4c
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-05-25 09:45:01 -0700

    switch to using SendGrid for the email provider

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.smtp_settings = {
-  address:              "smtp.gmail.com",
+  address:              "smtp.sendgrid.net",
   port:                 "587",
   enable_starttls_auto: true,
   authentication:       :plain,
