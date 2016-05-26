90fef2e5020b3ac8864253200670bf8ec8e949a9
owen flood <oneflood@owens-MacBook-Pro.local>
2016-05-26 12:09:41 -0700

    add fetch organization news job and news search service object

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,6 +1,8 @@
 class NewsArticle < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
 
+  has_many :organizations, through: :organization_news
+
   def self.recent_news
     where("created_at > ?", DateTime.now - 1.month).order("published_on DESC")
   end
---
+++ lib/tasks/fetch_organization_news.rake
@@ -0,0 +1,6 @@
+namespace :fetch_organization_news do
+  desc "Fetching news articles from google news that are specific for companies"
+  task :fetch_all => :environment do
+    FetchNewsJob.perform_now
+  end
+end
