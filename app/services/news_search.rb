f57ef05ac21153a81280fbb4f9f279ffd8c92e28
owen flood <oneflood@owens-MacBook-Pro.local>
2016-05-26 12:09:41 -0700

    add fetch organization news job and news search service object

---
+++ app/services/news_search.rb
@@ -0,0 +1,32 @@
+class NewsSearch
+  def search(search_term)
+    article_ids = []
+    @news = GoogleCustomSearchApi.search(@search_term, page: 1)
+    @search_term = search_term
+    byebug
+    @news.items.each do |item|
+      parser = ArticleParser.new(item)
+
+      if parser.published_on
+        article = NewsArticle.new(
+          title: parser.title,
+          snippet: parser.snippet,
+          link: parser.link,
+          thumbnail: parser.thumbnail,
+          published_on: parser.published_on
+        )
+      else
+        Rails.logger = Logger.new(STDOUT)
+        Rails.logger.info "Not a real article"
+      end
+
+      if article && article.save
+        article_ids.push(article.id)
+        Rails.logger.info "Article saved"
+      else
+        Rails.logger.info "Article not saved"
+      end
+    end
+    return article_ids
+  end
+end
---
+++ spec/factories/organization_news.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :organization_news do
+    news_article nil
+    organization nil
+  end
+end
