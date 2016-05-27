5bbda721d3ccc10d96b29de406ba4233b30d61f2
yiivan <yiivan@hotmail.com>
2016-05-27 11:45:11 -0700

    delete claim req when deleting user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   belongs_to :organization
-  has_many :claim_request, dependent: :nullify
+  has_many :claim_request, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
