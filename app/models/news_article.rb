b0fde0df833a1ff6d168756616637345e35851d2
owen flood <oneflood@owens-MacBook-Pro.local>
2016-05-31 09:31:28 -0700

    Fix rspec for fetch news job and associate organization news to their organization and article

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -2,6 +2,7 @@ class NewsArticle < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
 
   has_many :organizations, through: :organization_news
+  has_many :organization_news, dependent: :destroy
 
   def self.recent_news
     where("created_at > ?", DateTime.now - 1.month).order("published_on DESC")
