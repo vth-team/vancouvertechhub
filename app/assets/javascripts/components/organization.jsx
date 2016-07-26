f96746008824e07880c641a7fcb99adf2efc48a6
Makoto Ejima <makotoejima@gmail.com>
2016-07-26 10:12:47 -0700

    search by term and team size working concurrently

--- app/assets/javascripts/components/organization.jsx
+++ app/assets/javascripts/components/organization.jsx
@@ -1,6 +1,6 @@
 var Organization = React.createClass({
   render: function() {
-    return <div style={styles} className="org-container col-lg-3 col-md-4 col-sm-6 col-xs-12 text-center" data-id={ this.props.organization.id } >
+    return <div className="org-container col-lg-3 col-md-4 col-sm-6 col-xs-12 text-center" data-id={ this.props.organization.id } >
               <img src={ this.props.organization.logo } id="org-logo"/>
               <h3><a href={ "/organizations/" + this.props.organization.id } className="link-to-org">{ this.props.organization.name }</a></h3>
               <p>{ this.props.organization.employee_count } employees</p>
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,31 +2,34 @@ class SearchController < ApplicationController
 
    # The organization_display.jsx react component sends an ajax request with the appropriate parameters
   def search
-    @results = []
     if params[:term]
-      @results.concat(Organization.where('name ILIKE ? OR overview ILIKE ?', "%#{params[:term]}%", "%#{params[:term]}%"))
+      term = Organization.where('name ILIKE ? OR overview ILIKE ?', "%#{params[:term]}%", "%#{params[:term]}%")
     end
     if params[:size]
       if params[:size] == "1"
-        @results.concat(Organization.where(tech_team_size: 1..25))
+        size = Organization.where(tech_team_size: 1..25)
       elsif params[:size] == "2"
-        @results.concat(Organization.where(tech_team_size: 26..50))
+        size = Organization.where(tech_team_size: 26..50)
       elsif params[:size] == "3"
-        @results.concat(Organization.where('tech_team_size > ?', 50))
+        size = Organization.where('tech_team_size > ?', 50)
+      else
+        size = Organization.all
       end
     end
-    if params[:tech]
-      # This gets the ids of the specified techs
-      techs = params[:tech].split(' ').map{|x| x.to_i}
-      org_ids = OrganizationTechnology.where(technology_id: techs).pluck('organization_id').uniq
-      org_ids.each do |o|
-        @results.concat(Organization.where(id: o))
-      end
-    end
+    # if params[:tech]
+    #   # This gets the ids of the specified techs
+    #   techs = params[:tech].split(' ').map{|x| x.to_i}
+    #   org_ids = OrganizationTechnology.where(technology_id: techs).pluck('organization_id').uniq
+    #   org_ids.each do |o|
+    #     @results.concat(Organization.where(id: o))
+    #   end
+    # end
+    @results = term & size & tech
     respond_to do |format|
       format.html { render json: @results }
       format.json { render json: @results }
     end
   end
-end
 
+
+end
