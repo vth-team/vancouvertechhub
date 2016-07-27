d1a8b96a1a958d005bab7ac15892bf832da16dc1
Wei Yi <weiyi2017@gmail.com>
2016-07-27 10:29:30 -0700

    add rspec test for meetup crawl job

--- app/jobs/daily_meetup_crawl_job.rb
+++ app/jobs/daily_meetup_crawl_job.rb
@@ -7,19 +7,18 @@ class DailyMeetupCrawlJob < ActiveJob::Base
     @meet = meetup_api.method_request('self/calendar', {})
 
     @meet.each do |list|
-      if list["venue"]
-      url = list["link"]
-      address = list["venue"]["name"] + ", " + list["venue"]["address_1"] + ", " + list["venue"]["city"]
-      title = list["name"]
-      time = Time.at(list["time"]/1000).strftime("%Y-%m-%d %I:%M:%S")
-      event = Event.new
-      event.meetup_title = title
-      event.start_time = time
-      event.meetup_url = url
-      event.location = address
-      event.save
+        if list["venue"]
+          url = list["link"]
+          address = list["venue"]["name"] + ", " + list["venue"]["address_1"] + ", " + list["venue"]["city"]
+          title = list["name"]
+          time = Time.at(list["time"]/1000).strftime("%Y-%m-%d %I:%M:%S")
+          event = Event.new
+          event.meetup_title = title
+          event.start_time = time
+          event.meetup_url = url
+          event.location = address
+          event.save
+        end
     end
-  end
-
   end
 end
