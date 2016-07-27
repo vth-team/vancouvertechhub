8eeda1c578a8f9b58b01fb3108dc58e6997a8456
Yat Chang <yatmchang@gmail.com>
2016-07-27 11:23:15 -0700

    added testing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it "requires an unique meetup_url" do
+      e = Event.create(meetup_url:"www.meetup.com")
+      e2 = Event.new(meetup_url:"www.meetup.com")
+      expect(e2).to be_invalid
+    end
+  end
+
 end
