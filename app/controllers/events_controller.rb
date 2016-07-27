87c1a1ac8ba37fc37556351075b2a0b2818ae89d
Eric Richardson <eric@ericonrails.com>
2016-07-27 11:12:30 -0700

    Fix add event feature for admin. Change flash: to notice:

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      redirect_to admin_events_path, flash: "Event Added"
+      redirect_to admin_events_path, notice: "Event Added"
     else
       redirect_to admin_events_path, alert: "Event Not Added"
     end
@@ -16,7 +16,7 @@ class EventsController < ApplicationController
   def destroy
     @event = Event.find params[:id]
     if @event.destroy
-      redirect_to admin_events_path, flash: "Event Removed"
+      redirect_to admin_events_path, notice: "Event Removed"
     else
       redirect_to admin_events_path, alert: "Event Not Removed"
     end
