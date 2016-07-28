c5c1042ec123a785ccaee20b693d33ceb8bb9a7d
Yat Chang <yatmchang@gmail.com>
2016-07-28 11:05:50 -0700

    made Event Model Testing more specific

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Event, type: :model do
     it "requires an unique meetup_url" do
       e = Event.create(meetup_url:"www.meetup.com")
       e2 = Event.new(meetup_url:"www.meetup.com")
-      expect(e2).to be_invalid
+      e2.valid?
+      expect(e2.errors).to have_key(:meetup_url)
     end
   end
 
